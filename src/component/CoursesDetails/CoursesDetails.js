428107c813b9c0cea5949b0bd14a64d36c131f70
Akahad <aksahad234567@gmail.com>
2022-10-27 18:16:22 +0600

    react-pdf add

--- src/component/CoursesDetails/CoursesDetails.js
+++ src/component/CoursesDetails/CoursesDetails.js
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
+import ReactPrint from "react-to-print"
+import ReactToPrint from 'react-to-print';
 
 const CoursesDetails = () => {
   const {them}=useContext(AuthContext)
     const data=useLoaderData()
     const {name,titel,img,body,id}= data
+    const ref = useRef()
     
     return (
-        <div id={them} className=' d-flex justify-content-center align-items-center'> 
+        <div ref={ref} id={them} className=' d-flex justify-content-center align-items-center'> 
             <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={img} />
       <Card.Body>
@@ -19,7 +22,8 @@ const CoursesDetails = () => {
           {body}
         </Card.Text>
         <Button variant="dark"><Link to={`/chackout/${id}`} >Get premiumaess</Link></Button>
-        <Button className='mx-3' variant="dark">Dowloaed pdf</Button>
+        <ReactToPrint trigger={()=><Button className='mx-3' variant="dark">Dowloaed pdf</Button>} content={()=>ref.current}> </ReactToPrint>
+        
         
       </Card.Body>
     </Card>
